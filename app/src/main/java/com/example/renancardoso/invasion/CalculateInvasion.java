8dfeef1a7afb501d0c7db45c3d29fc08f194a732
Renan Augusto Cardoso <augustocardoso07@gmail.com>
2015-11-10 23:59:29 -0200

    CalculateInvasion.java working and some UI added

---
+++ app/src/main/java/com/example/renancardoso/invasion/CalculateInvasion.java
@@ -0,0 +1,114 @@
+package com.example.renancardoso.invasion;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+/**
+ * Created by renan on 10/11/15.
+ */
+public class CalculateInvasion {
+    private int mM;
+    private int mN;
+    private String[] mMs;
+    private String[] mNs;
+    private CharSequence mDate;
+    private String method;
+    final private String[] DAYS = {"31",  "1",  "2",  "3",  "4",  "5",  "6",  "7",  "8",  "9", "10",
+            "11", "12", "13", "14", "15", "16", "17", "18", "19", "20",
+            "21", "22", "23", "24", "25", "26", "27", "28", "29", "30"};
+    final private String[] MONTHS = {"dezembro", "janeiro", "fevereiro", "março", "abril", "maio",
+            "junho", "julho", "agosto", "setembro", "outubro", "novembro"};
+
+    public CalculateInvasion(int M,int N,String[] Ms, String[] Ns) {
+        mM = M;
+        mN = N;
+        mMs = Ms;
+        mNs = Ns;
+
+        if (defineMethod().equals("A")) mDate = methodA();
+        else                            mDate = methodB();
+
+    }
+
+    public CharSequence getDate() {
+        return mDate;
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    private String defineMethod() {
+        int sum = 0;
+        for (String s : mMs) {
+            sum += Integer.parseInt(s);
+        }
+
+        if ((sumDigits(sum) % 2) == 0) method = "A";
+        else                           method = "B";
+
+        return method;
+    }
+
+    private int sumDigits(int number) {
+        int n = number;
+        int sum = 0;
+        while (n > 0) {
+            int lastDigit = n % 10;
+            n = n / 10;
+            int penultDigit = n % 10;
+            sum += lastDigit + penultDigit;
+            n = n / 10;
+        }
+        return sum;
+    }
+
+    private CharSequence methodA() {
+        int S = 0;
+        ArrayList<Integer> ns = new ArrayList<>();
+
+        for (String s: mNs) {
+            ns.add(Integer.parseInt(s));
+        }
+
+        Collections.sort(ns);
+
+        for (int i = 0; i < mN; i++) {
+            int n = ns.get(i);
+            int produto = n * (i + 1);
+            S += produto;
+        }
+        return formatDate(S);
+    }
+
+    private CharSequence formatDate(int S) {
+        int day   = S % 31;
+        int month = S % 12;
+        return DAYS[day] + " de " + MONTHS[month];
+    }
+
+    private CharSequence methodB() {
+        //
+        int count = 1;
+        int[] partial = new int[mN];
+
+        for (int i = 0; i < mNs.length; i++) {
+            count += i;
+            partial[i] = Integer.parseInt(mNs[i]) + count;
+        }
+
+        int S = sumLastTwoDigits(partial);
+        return formatDate(S);
+    }
+
+    private int sumLastTwoDigits(int[] numbers) {
+        int sum = 0;
+        for (int number: numbers) {
+            int lastDigit = number % 10;
+            int penultDigit = (number / 10) % 10;
+            sum += lastDigit + penultDigit;
+        }
+        return sum;
+    }
+}
+
--- app/src/main/java/com/example/renancardoso/invasion/MainActivity.java
+++ app/src/main/java/com/example/renancardoso/invasion/MainActivity.java
@@ -1,14 +1,20 @@
 package com.example.renancardoso.invasion;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.accessibility.AccessibilityManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.Arrays;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -23,12 +29,12 @@ public class MainActivity extends ActionBarActivity {
 
 		Button BtnBackspace = (Button) findViewById(R.id.textViewBackspace);
 		BtnBackspace.setOnLongClickListener(new View.OnLongClickListener() {
-			@Override
-			public boolean onLongClick(View v) {
-				textViewCode.setText("");
-				return true;
-			}
-		});
+            @Override
+            public boolean onLongClick(View v) {
+                textViewCode.setText("");
+                return true;
+            }
+        });
 	}
 
 	@Override
@@ -82,6 +88,93 @@ public class MainActivity extends ActionBarActivity {
 	}
 
 	public void calculateInvasion(View v) {
-		Toast.makeText(getApplicationContext(), "Teste", Toast.LENGTH_LONG).show();
+        try {
+            String[] arr = textViewCode.getText().toString().trim().split("\\s+");
+            if (!validateArr(arr)) {
+                showErr("O códico está incorreto. Por favor, digite novamente");
+                return;
+            }
+
+            int M = Integer.parseInt(arr[0]);
+            if (!validateM(M)) {
+                showErr("O primeiro número deve estar entre 1 e 5");
+                return;
+            }
+
+            int N = Integer.parseInt(arr[1]);
+            if (!validateN(N)) {
+                showErr("O segundo número deve estar entre 1 e 10");
+                return;
+            }
+
+            String[] Ms = Arrays.copyOfRange(arr, 2, 2 + M);
+            if (!validateMs(Ms, M)) {
+                showErr("A segunda parte do código deve conter " + M + " elementos");
+                return;
+            }
+
+            String[] Ns = Arrays.copyOfRange(arr, 2 + M, arr.length);
+            if (!validateNs(Ns, N)) {
+                showErr("A terceira parde do código deve conter " + N + " elementos");
+                return;
+            }
+
+
+            CalculateInvasion invasion = new CalculateInvasion(M, N, Ms, Ns);
+            openDialog(invasion.getDate());
+        } catch (java.lang.NumberFormatException e) {
+            showErr("Número invalido");
+        } catch (java.lang.IndexOutOfBoundsException e) {
+            showErr("Array invalida");
+        } catch (Exception e) {
+            showErr("Erro desconhecido");
+        }
+
 	}
+
+    private boolean validateArr(String[] arr) {
+        int sum = (Integer.parseInt(arr[0]) + Integer.parseInt(arr[1]));
+        boolean part1 = arr.length >= 4;
+        boolean part2 = sum + 2 == arr.length;
+        boolean result =  part1 && part2;
+        return result;
+    }
+
+    private void showErr(CharSequence text) {
+        Toast.makeText(getApplicationContext(), text, Toast.LENGTH_LONG).show();
+    }
+
+    private boolean validateM(int M) {
+        return M >= 1 && M <= 5;
+    }
+
+    private boolean validateN(int N) {
+        return N >= 1 && N <= 10;
+    }
+
+    private boolean validateMs(String[] Ms, int M) {
+        for (String s: Ms) if (Integer.parseInt(s) >= 500) return false;
+        return Ms.length == M;
+    }
+
+    private boolean validateNs(String[] Ns, int N) {
+        return  Ns.length == N;
+    }
+
+    private void openDialog(CharSequence text) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setTitle("Data da Invasão:");
+        builder.setMessage(text);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getApplicationContext(), "Código decifrado com succeso!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        AlertDialog alert = builder.create();
+        alert.show();
+
+    }
 }
